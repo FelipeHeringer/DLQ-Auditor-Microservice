9b0a4c12e23148533cf564ea2a667721e4636a9b
FelipeHeringer <heringerfelipe14@gmail.com>
2026-05-12 20:34:35 -0300

    chore: adicionar estrutura do projeto spring

---
+++ dlq-auditor/src/main/java/com/fhcs/dlq/auditor/DlqAuditorApplication.java
@@ -0,0 +1,13 @@
+package com.fhcs.dlq.auditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlqAuditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlqAuditorApplication.class, args);
+	}
+
+}
---
+++ dlq-auditor/src/test/java/com/fhcs/dlq/auditor/DlqAuditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhcs.dlq.auditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlqAuditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
